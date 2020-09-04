12ddc36d52252eec0b5c98b283305d9c337c561e
IgorBoroda <53015199+IgorBoroda@users.noreply.github.com>
2020-09-04 15:31:17 +0200

    Initial commit

---
+++ Demo/Scripts/MathCalcDemo.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleMan.Extensions;
+
+
+public class MathCalcDemo : MonoBehaviour
+{
+    //******     FIELDS AND PROPERTIES   	******\\
+    [SerializeField]
+    private float m_speed = 5;
+
+    [SerializeField]
+    private float m_distance = 5;
+
+
+    private Transform m_satellite;
+    private float m_angle;
+
+
+
+    //******    	    METHODS  	  	    ******\\
+    private void Start()
+    {
+        //Create satellite sphere
+        m_satellite = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+        m_satellite.parent = transform;
+    }
+
+    void Update()
+    {
+        //Increace angle
+        m_angle += m_speed * Time.deltaTime;
+
+
+        //Set satellite position
+        m_satellite.localPosition = MathCalculations.GetDirectionByAngle(m_distance, m_angle);
+    }
+}
---
+++ Scripts/MathCalculations.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SimpleMan.Extensions
+{
+    public static class MathCalculations
+    {
+        /// <summary>
+        /// Get equidistant points positions on circle
+        /// </summary>
+        /// <param name="_radius">Radius of the circle</param>
+        /// <param name="_pointsNumber">Number of points</param>
+        /// <returns></returns>
+        public static Vector2[] GetPointsOnCircle(float _radius, int _pointsNumber)
+        {
+            Vector2[] l_points = new Vector2[_pointsNumber];
+
+
+            float l_degrees = 360;
+            float l_angle = 360 / _pointsNumber;
+
+
+            for (int i = 0; i < _pointsNumber; i++)
+            {
+                l_points[i] = new Vector2((float)System.Math.Cos((l_degrees) * Mathf.Deg2Rad) * _radius,
+                                                    (float)System.Math.Sin((l_degrees) * Mathf.Deg2Rad) * _radius);
+
+
+                l_degrees -= l_angle;
+            }
+
+
+            return l_points;
+        }
+
+
+        /// <summary>
+        /// Get equidistant points positions on circle
+        /// </summary>
+        /// <param name="_center">Center of the circle</param>
+        /// <param name="_radius">Radius of the circle</param>
+        /// <param name="_pointsNumber">Number of points</param>
+        /// <returns></returns>
+        public static Vector2[] GetPointsOnCircle(Vector2 _center, float _radius, int _pointsNumber)
+        {
+            Vector2[] l_points = new Vector2[_pointsNumber];
+
+  
+            float l_degrees = 360;
+            float l_angle = 360 / _pointsNumber;
+
+
+            for (int i = 0; i < _pointsNumber; i++)
+            {
+                l_points[i] = _center + new Vector2((float)System.Math.Cos((l_degrees) * Mathf.Deg2Rad) * _radius,
+                                                    (float)System.Math.Sin((l_degrees) * Mathf.Deg2Rad) * _radius);
+
+
+                l_degrees -= l_angle;
+            }
+
+
+            return l_points;
+        }
+
+
+
+        
+
+
+        /// <summary>
+        /// Get point on circle by angle
+        /// </summary>
+        /// <param name="_length"> Radius of circle </param>
+        /// <param name="_angle"> Target angle </param>
+        /// <returns></returns>
+        public static Vector2 GetDirectionByAngle(float _length, float _angle)
+        {
+
+            //Coversion euler's angle to radians  
+            _angle *= Mathf.PI / 180;
+
+
+            //Set crosshair position
+            Vector2 l_position = new Vector2(_length * (float)Math.Cos(_angle),
+                                             _length * (float)Math.Sin(_angle));
+
+
+            return l_position;
+        }
+
+
+        /// <summary>
+        /// Get point on circle by angle
+        /// </summary>
+        /// <param name="_center"> Center of circle </param>
+        /// <param name="_radius"> Radius of circle </param>
+        /// <param name="_angle"> Target angle </param>
+        /// <returns></returns>
+        public static Vector2 GetPointOnCircle(Vector2 _center, float _radius, float _angle)
+        {
+            
+            //Coversion euler's angle to radians  
+            _angle *= Mathf.PI / 180;
+
+
+            //Set crosshair position
+            Vector2 l_position = new Vector2(_radius * (float)Math.Cos(_angle), 
+                                             _radius * (float)Math.Sin(_angle));
+
+
+            return l_position + _center;
+        }
+
+
+        /// <summary>
+        /// Get point on circle by angle
+        /// </summary>
+        /// <param name="_center"> Center of circle </param>
+        /// <param name="_radius"> Radius of circle </param>
+        /// <param name="_angle"> Target angle </param>
+        /// <returns></returns>
+        public static Vector2 GetPointOnCircle(float _radius, float _angle)
+        {
+
+            //Coversion euler's angle to radians  
+            _angle *= Mathf.PI / 180;
+
+
+            //Set crosshair position
+            Vector2 l_position = new Vector2(_radius * (float)Math.Cos(_angle),
+                                             _radius * (float)Math.Sin(_angle));
+
+
+            return l_position;
+        }
+    }
+}
